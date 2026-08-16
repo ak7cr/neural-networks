c1b14673e068513acd3ecf4d170d7ee4d388df96
Anshuman Kumar <141804301+ak7cr@users.noreply.github.com>
2026-08-16 17:25:19 +0530

    first commit

---
+++ one-neuron.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Neuron {
+    public :
+        vector <double> weights;
+        double bias;
+
+        Neuron(int input_size){
+            weights.resize(input_size, 0.0);
+            bias = 0.0;
+        }
+
+        double forward(const vector<double>&inputs){
+            double z= bias;
+            for(size_t i=0; i<inputs.size(); i++){
+                z += inputs[i] + weights[i]; 
+            }
+
+             return z;
+        }
+       
+
+};
+
+int main(){
+    Neuron neuron(3);
+
+    neuron.weights[0] = 0.5;
+    neuron.weights[1] = 0.2;
+    neuron.weights[2] = 0.8;
+
+    neuron.bias = 1.0;
+
+    vector<double> input = {2, 3, 4};
+
+    double output = neuron.forward(input);
+
+    cout << output;
+    
+}
